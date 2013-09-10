0fce73f5d99bce27c2d381d15885d1a8b14cf0bd
Michael <michaeldigit@gmail.com>
2013-09-10 10:55:21 -0700

    clean up

--- FrenchVocab/src/com/mck/vocab/fragments/EasyDialogQuestionFragment.java
+++ FrenchVocab/src/com/mck/vocab/fragments/EasyDialogQuestionFragment.java
@@ -1,6 +1,5 @@
 package com.mck.vocab.fragments;
 import android.app.Activity;
-import android.content.res.Resources.Theme;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
